198eca3fc1b23128a9c5fb43b65731955d021e98
Hayanna Castro <152522247+hayannacastro@users.noreply.github.com>
2025-06-02 15:14:29 -0300

    Add files via upload

---
+++ ProjetoFormasGeometricasEncapsuladas/FormasGeometricas/src/br/edu/formasgeometricasplanas/Quadrado.java
@@ -0,0 +1,28 @@
+package br.edu.formasgeometricasplanas;
+public class Quadrado {
+    
+	private double lado;
+    private double area;
+
+    public Quadrado() {
+    	lado = 0;
+    	area = 0;
+    	
+    } public void setLado(double Lado) {
+        lado = Lado;
+        
+    } public double getLado() {
+        return lado;
+        
+    } public void calcArea() {
+        area = lado * lado;
+        
+    } public void mostrarArea() {
+        System.out.println("Área: " + area);
+        
+    } public double getArea() {
+        System.out.println("Área com retorno: " + area);
+        return area;
+    }
+}
+
